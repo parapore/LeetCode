58d71494854ee2b2ae1eef1e8cb212dc6cca5dc3
parapore <eriglai577@gmail.com>
2023-06-01 21:38:55 +0900

    最大公約数問題。ユークリッドの互除法。修正。

--- LeetCode/src/atcoder/CubicCake.java
+++ LeetCode/src/atcoder/CubicCake.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class CubicCake {
 
 	public static void main(String[] args) {
-		public static void main(String[] args) {
 			Scanner sc = new Scanner(System.in);
 			long aa = sc.nextLong();
 			long bb = sc.nextLong();
