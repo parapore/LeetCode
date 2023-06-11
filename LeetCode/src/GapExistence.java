35a620349f93910db4d2533bd731bc7666e9fa32
parapore <eriglai577@gmail.com>
2023-06-11 18:42:07 +0900

    引き算。HashSetで計算量を減らす問題。

---
+++ LeetCode/src/GapExistence.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class GapExistence {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int x = sc.nextInt();
+		Set<Integer> set = new HashSet<>();
+
+		for (int i = 0; i < n; i++) {
+			set.add(sc.nextInt());
+		}
+
+		//Setでも後で作ってみよう。
+		for(int num : set) {
+			int aj = num -x;
+			if(set.contains(aj)) {
+				System.out.println("Yes");
+				return;
+			}
+		}
+		System.out.println("No");
+	}
+}
